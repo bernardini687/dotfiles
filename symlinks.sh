74adc3d0d135a5db56c4f1a0672ecc3127113b2d
oscar bernardini <bernardini687@gmail.com>
2019-12-31 16:13:09 +0100

    update linking script

--- symlinks.sh
+++ symlinks.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+
 for dir in *; do
-  if [ -d "$dir" ]; then
+  if [ -d $dir ]; then
     cd $dir
     for config in *; do
-      if [ -f "$config" ]; then
-        target="$HOME/.$config"
-        echo "--- Symlinking $target"
-        ln -s "$PWD/$config" $target
+      if [ -f $config ]; then
+        target=$HOME/.$config
+        if [ -f $target ]; then
+          echo "  [X] - $target -> already linked!"
+        else
+          echo "  [.] - $target -> creating link..."
+          ln -s $PWD/$config $target
+        fi
       fi
     done
     cd ..
