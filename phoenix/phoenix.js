313f343a3932b4f9a9b3e672422c123cbfd70e65
oscar bernardini <bernardini687@gmail.com>
2020-03-29 18:03:27 +0200

    add phoenix configuration

---
+++ phoenix/phoenix.js
@@ -0,0 +1,26 @@
+Phoenix.set({
+  daemon: true,
+  openAtLogin: true
+})
+
+function golden (length) {
+  return Math.round((61.8 / 100) * length)
+}
+
+function goldenFrame (percentage) {
+  const { width, height } = Screen.main().frame()
+
+  const goldenWidth = golden(width)
+  const goldenHeight = golden(height)
+
+  return {
+    x: width / 2 - goldenWidth / 2,
+    y: height / 2 - goldenHeight / 2,
+    width: goldenWidth,
+    height: goldenHeight
+  }
+}
+
+const goldenWindow = new Key('f', ['control', 'shift'], () => {
+  Window.focused().setFrame(goldenFrame())
+})
